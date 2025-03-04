1bcb511152b1609e025b56f4e023c9bd30f857d2
zsm1211 <444928118@qq.com>
2025-03-04 21:45:14 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+import torch
+import scipy.io as scio
+import numpy as np
+import math
+
+
+def A(x,Phi):
+    temp = x*Phi
+    y = torch.sum(temp,1)
+    return y
+
+def At(y,Phi):
+    temp = torch.unsqueeze(y, 1).repeat(1,Phi.shape[1],1,1)
+    x = temp*Phi
+    return x
